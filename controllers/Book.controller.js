561ff10ff505f9fb045d95cfe5bea30d373d7477
ThaerJomhawi <thaer.fjomhawi@gmail.com>
2021-08-18 17:09:19 +0300

    finish lab 12

--- controllers/Book.controller.js
+++ controllers/Book.controller.js
@@ -1,18 +1,26 @@
 "use strict";
+const getBookController = require("./getBook.controller");
 const BookModel = require("../modals/Book.model");
 
 const bookController = (req, res) => {
   const newBook = new BookModel(
          {
-        userEmail: "Math@ABU.jo",
-        bookTitle: "Linear Mathematical Models In Chemical Engineering",
+        userEmail: "Thaer.chemical@ABU.jo",
+        bookTitle: "Thaer for Chemical Engineering",
         bookDescription:
-          "Mathematics remains a core area of engineering. Formulating and analyzing mathematical models of basic engineering systems is an essential skill that all engineering students should endeavor to acquire.",
+          "thaer thaer thaer .",
         bookStatus: "Available",
       }
   )
 
   newBook.save();
-  res.send(newBook);
-};
+  BookModel.find({},(error,result)=>{
+    if (error){
+        res.send(error)
+    }
+    else{
+        res.send(result)
+    }
+}
+  )};
 module.exports = bookController;
